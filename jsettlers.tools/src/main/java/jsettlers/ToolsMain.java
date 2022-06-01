a6cce4d4dbde78617d0ce63acb8ede3790f4ad03
hiran <hiran@homeserver.indernet>
2022-06-02 00:31:04 +0200

    Reduce number of clicks for tool selection

--- jsettlers.tools/src/main/java/jsettlers/ToolsMain.java
+++ jsettlers.tools/src/main/java/jsettlers/ToolsMain.java
@@ -1,31 +1,76 @@
 package jsettlers;
 
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import jsettlers.buildingcreator.editor.BuildingCreatorApp;
 import jsettlers.graphics.debug.DatFileViewer;
 import jsettlers.logic.movable.MovableModelWindow;
 
 import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
 
 public class ToolsMain {
 
 	public static void main(String[] args) throws Exception{
-		Tool start = (Tool) JOptionPane.showInputDialog(null, "Select Tool", "Tool selection", JOptionPane.QUESTION_MESSAGE, null, Tool.values(), null);
-		start.mainFunc.run(args);
+                
+                JFrame f = new JFrame("Select Tool...");
+                f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                f.setLayout(new GridBagLayout());
+                
+                int y = 0;
+                for (Tool t: Tool.values()) {
+                    JButton b = new JButton(t.name(), t.icon);
+                    b.setHorizontalAlignment(SwingConstants.LEFT);
+                    b.addActionListener(e -> {
+                            f.setVisible(false);
+                            new Thread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    try {
+                                        t.mainFunc.run(args);
+                                    } catch (Exception ex) {
+                                        System.err.println("ERROR: Could not invoke tool "+t);
+                                        ex.printStackTrace(System.err);
+                                        f.setVisible(true);
+                                    }
+                                }
+                            }).start();
+                    });
+                    f.add(b, new GridBagConstraints(0, y++, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+                }
+                
+                f.pack();
+                f.setLocationRelativeTo(null);
+                f.setVisible(true);
 	}
 
 
 	private enum Tool {
 
-		DAT_FILE_VIEWER(DatFileViewer::main),
-		MOVABLE_MODEL_WINDOW(MovableModelWindow::main),
-		BUILDING_CREATOR(BuildingCreatorApp::main),
+		DAT_FILE_VIEWER(DatFileViewer::main, "/images/photo_library_FILL0_wght400_GRAD0_opsz48.png"),
+		MOVABLE_MODEL_WINDOW(MovableModelWindow::main, "/images/groups_FILL0_wght400_GRAD0_opsz48.png"),
+		BUILDING_CREATOR(BuildingCreatorApp::main, "/images/other_houses_FILL0_wght400_GRAD0_opsz48.png"),
 		;
 
 
 		public final MainFunc mainFunc;
+                public final ImageIcon icon;
 
-		private Tool(MainFunc mainFunc) {
+                /**
+                 * Creates a new Tool enum.
+                 * 
+                 * @param mainFunc the main function to call for this tool
+                 * @param relpath the classpath icon resource
+                 */
+		private Tool(MainFunc mainFunc, String relpath) {
 			this.mainFunc = mainFunc;
+                        this.icon = new ImageIcon(getClass().getResource(relpath));
 		}
 	};
 
